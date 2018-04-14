ca852c414cd12d9b0441ad25520559709117f635
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-14 10:58:15 -0600

    render mock cards

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Deck from './src/Deck';
+
+const data = [
+  { id: 1, text: 'Did you exercise today?', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Did you take your medicine?', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Question 3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Question 4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 6, text: 'Question 5', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+];
 
 export default class App extends React.Component {
+
+  renderCard(item) {
+    return (
+      <Text>{item.text}</Text>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>GLOW</Text>
+        <Deck
+          data={data}
+          renderCard={this.renderCard}/>
       </View>
     );
   }
