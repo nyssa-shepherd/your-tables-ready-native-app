df3a8290c08d2c94be6780101ab9c245256d5345
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-14 14:21:13 -0600

    stack cards

--- src/Deck/index.js
+++ src/Deck/index.js
@@ -84,14 +84,19 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers}>
               {this.props.renderCard(item)}
           </Animated.View>
         );
       }
-      return this.props.renderCard(item);
-    });
+      return (
+        <Animated.View key={item.id} style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </Animated.View>
+
+      )
+    }).reverse();
   }
 
   render(){
@@ -104,7 +109,9 @@ class Deck extends Component {
 }
 
 const styles = {
-  class: {
+  cardStyle: {
+    position: 'absolute',
+    width:SCREEN_WIDTH
   }
 }
 
