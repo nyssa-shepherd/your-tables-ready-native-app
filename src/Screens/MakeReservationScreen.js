7195072ba101cfb11b2fbd7b937d8fb0118f9e62
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 23:49:51 -0600

    Create a form for Make a Reservation Tab, missing a button still

--- src/Screens/MakeReservationScreen.js
+++ src/Screens/MakeReservationScreen.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 class MakeReservationScreen extends Component {
   static navigationOptions = {
     title: 'Make a Reservation'
   };
-  
+
   render() {
     return (
       <View>
-        <Text>Make Res</Text>
+        <Text>Make a Reservation</Text>
+        <TextInput style={styles.input}
+                   placeholder='Name'/>
+        <TextInput style={styles.input}
+                   placeholder='Date'/>
+        <TextInput style={styles.input}
+                   placeholder='Time'/>
+        <TextInput style={styles.input}
+                   placeholder='Number of People'/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'pink',
+    fontSize: 24,
+    height: 60,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 10,
+    width: 350
+  }
+});
 
 export default MakeReservationScreen;
