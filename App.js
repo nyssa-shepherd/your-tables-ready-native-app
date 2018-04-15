9fa1c128f6fb405a6fd3849746c13281504b0e42
Nyssa Keller <nyssa1@gmail.com>
2018-04-15 10:19:28 -0600

    Changes  i didnt commit yesterday and dont remember what they are oops

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Card, Button } from 'react-native-elements';
+
+import Home from './src/Screens/Home';
+import MakeReservationScreen from './src/Screens/MakeReservationScreen';
+import UpcomingReservationScreen from './src/Screens/UpcomingReservationScreen';
 
 export default class App extends React.Component {
 
   render() {
+    const MainNavigator = TabNavigator({
+      home: { screen: Home },
+      upcomingReservation: { screen: UpcomingReservationScreen },
+      makeReservation: { screen: MakeReservationScreen }
+    },
+    {
+      swipeEnabled: true,
+      lazy: false, 
+    });
+
     return (
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50
+    marginTop: 50,
+    flex: 1
   },
 });
