cde4c4363b446aab485ae1f17e62a8b9598b5134
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 23:32:36 -0600

    Create tab navigation

---
+++ src/Screens/MakeReservationScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MakeReservationScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Make Res</Text>
+      </View>
+    )
+  }
+}
+
+export default MakeReservationScreen;
