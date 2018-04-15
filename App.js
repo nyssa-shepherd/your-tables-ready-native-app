90817662d02b9cd7f86bc42eacd1641605bf5be5
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 20:34:35 -0600

    First commit

--- App.js
+++ App.js
@@ -1,56 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Button } from 'react-native-elements';
-import Deck from './src/Deck';
-
-const data = [
-  { id: 1, text: 'Did you exercise today?', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
-  { id: 2, text: 'Did you take your medicine?', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 3, text: 'Question 3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
-  { id: 4, text: 'Question 4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
-  { id: 6, text: 'Question 5', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-];
 
 export default class App extends React.Component {
 
-  renderCard(item) {
-    return (
-      <Card
-        key={item.id}
-        title={item.text}
-        image={{ uri: item.uri }}
-        >
-        <Text style={{ marginBottom: 10 }}>
-          Jorge
-        </Text>
-        <Button
-          icon={{ name: 'code' }}
-          backgroundColor="#03A9F4"
-          title="View Now!"
-        />
-      </Card>
-    )
-  }
-
-  renderNoMoreCards() {
-    return (
-      <Card title='No more cards'>
-        <Text style={{ marginBottom: 10 }}>
-          There are no more cards.
-        </Text>
-      </Card>
-    )
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Deck
-          data={data}
-          renderCard={this.renderCard}
-          renderNoMoreCards={this.renderNoMoreCards}
-        />
-      </View>
+      <View style={styles.container}></View>
     );
   }
 }
@@ -58,9 +14,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     marginTop: 50
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
